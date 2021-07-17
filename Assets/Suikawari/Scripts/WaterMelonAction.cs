531392906447026207547af8694b655a09f0ad05
ishide_motoki <cf.m1t0k@gmail.com>
2021-07-18 02:31:36 +0900

    add watermelon interaction

---
+++ Assets/Suikawari/Scripts/WaterMelonAction.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterMelonAction : MonoBehaviour, IInteractable
+{
+    [SerializeField]
+    Rigidbody separatedCollider_L, separetedCollider_R;
+
+    public void Interact()
+    {
+        separatedCollider_L.isKinematic = false;
+        separetedCollider_R.isKinematic = false;
+    }
+}
