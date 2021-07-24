decfa76ba5774ad5bd0b3d8243180c1553ecbf7f
ishide_motoki <cf.m1t0k@gmail.com>
2021-07-24 20:54:40 +0900

    スタート時のスイカの初期位置のランダム化

---
+++ Assets/Suikawari/Scripts/SuikaRandomMove.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuikaRandomMove : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 min, max;
+
+    private void Awake()
+    {
+        var x = Random.Range(min.x, max.x);
+        var z = Random.Range(min.z, max.z);
+
+        transform.position = new Vector3(x, 0, z);
+    }
+}
